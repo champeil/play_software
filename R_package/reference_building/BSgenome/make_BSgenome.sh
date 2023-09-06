417497c77aee416ad8ce9bb1f52f94336f8712cd
Champeil <33405808+champeil@users.noreply.github.com>
2023-09-06 14:42:33 +0800

    Create make_BSgenome.sh

---
+++ R_package/reference_building/BSgenome/make_BSgenome.sh
@@ -0,0 +1,9 @@
+# this script is for making BSgenome and install in R
+# author: laojp
+# time: 2023.09.06
+# position: SYSUCC bioinformatic platform
+# usage: 
+#  make_BSgenome.sh [seed_file] [fa_dir] [output_dir]
+#  [fa_dir]: all_fa uncompressed in the dir
+
+
